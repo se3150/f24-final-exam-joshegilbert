6b983d57cc09384cd5a5057b3540b6f4052f92fa
joshegilbert <jgilb811@gmail.com>
2024-12-09 14:29:08 -0700

    got the basics

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -5,8 +5,18 @@ So that I can chat with my spy friends like a pro.
 
 Scenario: I can successfully encode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I add "Strength and honor!" to the inputfield "#letters"
+    When I select the 2 option for element "#shift-amount"
+    When I click on the button "#submit"
+    When I pause for 100ms
+    Then I expect that element "#decoded_message" contains the text "Uvtgpivj cpf jqpqt!"
 
 Scenario: I can successfully decode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I add "Uvtgpivj cpf jqpqt!" to the inputfield "#letters"
+    When I select the 1 option for element "#decoder-setting"
+    When I click on the element "p"
+    When I select the 2 option for element "#shift-amount"
+    When I click on the button "#submit"
+    When I pause for 100ms
+    Then I expect that element "#decoded_message" contains the text "Strength and honor!"
--- test_brute.py
+++ test_brute.py
@@ -1,18 +1,58 @@
 import pytest
 from brute import Brute
+from unittest.mock import Mock
+import random
 
 todo = pytest.mark.skip(reason='todo: pending spec')
 
 def describe_Brute():
 
-    @pytest.fixture
-    def cracker():
-        return Brute("TDD")
 
-    def describe_bruteOnce():
-        # write your test cases here
-        pass
+    def test_describe_bruteOnce():
+       crack = Brute("awesomesauce")
+       assert crack.bruteOnce("awesomesauce") == True
 
-    def describe_bruteMany():
-        # write your test cases here
-        pass
+    def test_describe_brute_init_string():
+       hash = Mock()
+       Brute.hash = hash
+       crack = Brute("awesomesauce")
+       assert hash.call_count == 1
+    
+    def test_describe_brute_init_int():
+       hash = Mock()
+       Brute.hash = hash
+       crack = Brute("1234567890")
+       assert hash.call_count == 1
+
+    def test_describe_hash():
+        hash = Mock()
+        Brute.hash = hash
+        crack = Brute("awesomesauce")
+        assert hash.call_count == 1
+    
+    def test_bruteOnce_manytimes():
+        for i in range(10):
+            alfabet = ("abcdefghijklmnopqrstuvwxyz")
+            random_number1 = random.randint(1, 25)
+            random_number2 = random.randint(1, 25)
+            random_number3 = random.randint(1, 25)
+            string = (alfabet[random_number1], alfabet[random_number2], alfabet[random_number3])
+            string = string[0] + string[1] + string[2]
+            crack = Brute(string)
+            assert crack.bruteOnce(string) == True
+
+    def test_describe_bruteMany_random():
+        crack = Brute("awesomesauce")
+        random = Mock()
+        crack.randomGuess = random
+        crack.bruteMany(1)
+        assert random.call_count == 1
+    
+    
+    def test_describe_bruteMany_hash():
+        crack = Brute("awesomesauce")
+        random = Mock()
+        crack.hash = random
+        crack.bruteMany(1)
+        assert random.call_count == 1
+        
